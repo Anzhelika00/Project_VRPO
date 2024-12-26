c9a779f6a36a72a88c8ebf810f44b0374fcb4f66
Anzhelika00 <sadovskay1711@gmail.com>
2024-12-27 00:01:26 +0300

    Update dealer.cpp

--- dealer.cpp
+++ dealer.cpp
@@ -4,4 +4,15 @@ using namespace std;
     {
         return 1 + (rand() % 10);
     }
+     vector<int> generate_dealer_cards()
+    {
+        vector<int> cards = { get_random_card(), get_random_card() };
+
+        while (get_card_values(cards) < 17)
+        {
+            cards.push_back(get_random_card());
+        }
+
+        return cards;
+    }
 }
