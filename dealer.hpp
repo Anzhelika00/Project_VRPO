659d1bbf9dbb63b5b771c5ea3c3e5f81e00daeca
Anzhelika00 <sadovskay1711@gmail.com>
2024-12-27 00:55:58 +0300

    Update dealer.hpp

--- dealer.hpp
+++ dealer.hpp
@@ -1,1 +1,19 @@
+#pragma once
+#include <cstdlib>
+#include <ctime>
+#include <vector>
+#include <iostream>
+#include <algorithm>
 
+using namespace std;
+{
+    int get_random_card();
+
+    vector<int> generate_dealer_cards();
+
+    int get_card_values(vector<int> cards);
+
+    void display_card_value(int card);
+
+    void print_dealer_cards(vector<int> cards, int dealer_total);
+}
